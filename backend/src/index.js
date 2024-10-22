7063ab464a12b89419a138e7679cd150560ebdc6
Sachin Mourya <122693233+SACHIN-MOURYA@users.noreply.github.com>
2024-10-22 20:32:55 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 export const app = express();
 
 
-app.use(cors({origin:'http://localhost:5173' ,
+app.use(cors({origin:'https://xenon-stack-t1-n9cf.vercel.app' ,
     optionsSuccessStatus: 200,
     preflightContinue: false,
     methods: "GET,POST,OPTIONS",
